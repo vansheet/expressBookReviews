4b9d6db8acf1d46b118d16d12327d128c1a6c7f1
vansheet <144224686+vansheet@users.noreply.github.com>
2023-09-26 21:10:09 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,7 +6,14 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  let userwithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userwithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
