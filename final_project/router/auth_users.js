adec128cb6113c7d1b406c61983241ac6cc25852
vansheet <144224686+vansheet@users.noreply.github.com>
2023-09-26 21:31:31 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -41,6 +41,19 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(404).json({message: "Unable to Display Book Review"});
 });
 
+// Delete a book review
+reg_users.delete("/auth/review/:isbn", (req,res) => {
+  const isbn = req.params.isbn;
+  let reviewer = req.session.authorization['username'];
+  let filtered_review = books[isbn]["reviews"];
+  if (filtered_review[reviewer]){
+    delete filtered_review[reviewer];
+    res.send('Reviews for the ISBN ${isbn} posted by the user ${reviewer} deleted.');
+  } else {
+    res.send("Can't delete, as this review has been posted by a different user");
+  }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
