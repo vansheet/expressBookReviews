68e5349f5f9fb9a10b1b526e114ffabce52e336d
vansheet <144224686+vansheet@users.noreply.github.com>
2023-09-27 19:20:34 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -14,16 +14,42 @@ const isValid = (username)=>{ //returns boolean
   } else {
     return false;
   }
-}
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validUser = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+    });
+    if(validUser.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    if (authenticatedUser(username, password)) {
+        let acessToken = jwt.sign(
+            {
+                data: password,
+            },
+            "access",
+            { expiresIn: 60*60 }
+        );req.session.authorization = {
+            accessToken,
+            username,
+        };
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password"});
+    }
 });
 
 // Add a book review
@@ -35,14 +61,18 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     let reviewer = req.session.authorization['username'];
     if(review) {
       filtered_book['reviews'][reviewer] = review;
-    } else {
-      res.send("Unable to find this ISBN!");
+      books[isbn] = filtered_book;
     }
+    res.send('The review for the book with ISBN ${isbn} has been added.');
+  } 
+  else {
+      res.send("Unable to find this ISBN!");
+  }
   return res.status(404).json({message: "Unable to Display Book Review"});
 });
 
 // Delete a book review
-reg_users.delete("/auth/review/:isbn", (req,res) => {
+regd_users.delete("/auth/review/:isbn", (req,res) => {
   const isbn = req.params.isbn;
   let reviewer = req.session.authorization['username'];
   let filtered_review = books[isbn]["reviews"];
